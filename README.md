51fb343a22bd9ca57ee62e336507b3dc8849f9ee
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-01-22 09:58:39 +0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # base64-2-safe
+[RFC 4648](https://tools.ietf.org/html/rfc4648#section-5) base64url simple implementation.
+## API
+### base64ToSafe(base64Str);
+- `base64Str` <String> base64 string.
+- Returns: url safe base64.
+## Demo
+```js
+const base64ToSafe = require('base64-2-safe');
+
+const result = base64ToSafe('a/bc+d==');
+console.log(result);
+// a_bc-d
+```
--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-// Asynchronous
-// buffer_buffers_and_character_encodings 
-// https://nodejs.org/api/buffer.html#buffer_buffers_and_character_encodings
-// nodejs: this encoding will also correctly accept "URL and Filename Safe Alphabet" 
-// is a lie: https://github.com/nodejs/node/blob/f0d2df41f8716670435b284e987b2fcc23221947/lib/buffer.js#L694-L698
+/**
+ * author: hezedu<hezedu@hotmail.com>
+ * license: MIT
+ * 
+ * nodeJs: this encoding will also correctly accept "URL and Filename Safe Alphabet"
+ * is a lie. https://nodejs.org/api/buffer.html#buffer_buffers_and_character_encodings
+ * 
+ * RFC: 
+ * https://tools.ietf.org/html/rfc4648#section-5
+ */
 
-// https://tools.ietf.org/html/rfc4648#section-5
-
-// Fast than tree regxp function: https://github.com/crypto-utils/uid-safe/blob/52edb678aebda31723ddc90048df07315408256b/index.js#L102-L107
+// Fast than tree regxp's function.
 
 const REGEXP = /\+|\//g;
 
 function base64ToSafe(base64){
-
-  // substr is fast than reg.
-  let result;
-  let lastIndex = base64.length - 2;
-  if(base64[lastIndex] !== '='){
-    lastIndex = lastIndex + 1;
-  }
-  if(base64[lastIndex] === '='){
-    result = base64.substr(0, lastIndex)
-  }
-
-  // use fn more fast than two reg.
-  return result.replace(REGEXP, function(mstr){
+  return _trimEndPading(base64).replace(REGEXP, function(mstr){
+    // use fn more fast than two reg.
     if(mstr === '+'){
       return '-';
     } else {
@@ -31,6 +23,18 @@ function base64ToSafe(base64){
       return '_';
     }
   });
+}
+
+function _trimEndPading(base64){
+  // substr is fast than reg.
+  let lastIndex = base64.length - 2;
+  if(base64[lastIndex] !== '='){
+    lastIndex = lastIndex + 1;
+  }
+  if(base64[lastIndex] === '='){
+    return base64.substr(0, lastIndex);
+  }
+  return base64;
 }
 
 module.exports = base64ToSafe;
